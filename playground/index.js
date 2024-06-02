ed9facd790e592bc1eb1758eb2d7c184e58ebb1f
Peder Frang <peder.frang@gmail.com>
2024-06-02 14:14:37 +0200

    cleanup node modules locally

--- playground/index.js
+++ playground/index.js
@@ -2,4 +2,3 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var my_first_npm_package_1 = require("@pjaff/my-first-npm-package");
 (0, my_first_npm_package_1.printInput)('Hei turid');
-console.log("202020");
--- playground/index.ts
+++ playground/index.ts
@@ -1,5 +1,3 @@
 import { printInput } from '@pjaff/my-first-npm-package'
 
 printInput('Hei turid')
-
-console.log("202020")
